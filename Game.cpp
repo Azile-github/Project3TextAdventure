0a5a005885c0819e19dcf8bee5f9645c29de4432
SkylaGyimesi <70960491+SkylaGyimesi@users.noreply.github.com>
2020-11-08 15:37:01 -0700

    Initial stuff
    Made game.cpp and had it load all the files/make them

---
+++ Game.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h> 
+
+using namespace std;
+
+int main(){
+  bool loadState = false;
+  ifstream highscore;
+  ifstream save;
+  ifstream items;
+  ifstream shopStatus;
+  highscore.open("highscore.dat");
+  save.open("save.dat");
+  items.open("items.dat");
+  shopStatus.open("shop.dat");
+  while (loadState == false){
+    highscore.open("highscore.dat");
+    save.open("save.dat");
+    items.open("items.dat");
+    shopStatus.open("shop.dat");
+    cout << "Checking files..." << endl;
+    if(!highscore.is_open() || !save.is_open() || !items.is_open() || !shopStatus.is_open()){
+      cout << "One or more file has failed to open..." << endl;
+      if(!highscore.is_open()){
+        cout << "Generating high score file..." << endl;
+        ofstream createHighscore;
+        createHighscore.open("highscore.dat");
+        createHighscore.close();
+      }
+      if(!save.is_open()){
+        cout << "Generating save file..." << endl;
+        ofstream createSave;
+        createSave.open("save.dat");
+        createSave.close();
+      }
+      if(!items.is_open()){
+        cout << "ERROR: Items file failure! Please redownload the game!" << endl;
+        return -1;
+      }
+      if(!shopStatus.is_open()){
+        cout << "Error: Shop file failure! Please redownload the game!" << endl;
+        return -1;
+      }
+    }
+  }
+}
