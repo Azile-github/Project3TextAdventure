51ed885d33c68e37466f87264c4bb0d7bc918919
ArchaicStupid <73682309+ArchaicStupid@users.noreply.github.com>
2020-11-19 13:40:01 -0700

    Add files via upload

---
+++ Item copy.h
@@ -0,0 +1,31 @@
+#ifndef ITEM_H
+#define ITEM_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Item{
+    private: 
+        string name;
+        string description;
+        int attack;
+        int damage;
+        
+    public: 
+
+        void setName(string name);
+        string getName();
+
+        void setDescription(string desc);
+        string getDescription();
+
+        void setAttack(int att);
+        int getAttack();
+
+        void setDamage(int dmg);
+        int getDamage();
+
+};
+#endif
