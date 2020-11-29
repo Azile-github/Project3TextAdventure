d50f2a765eb4a02e7d4b4ea6f634ecd0952f9407
AstroBlueX <pokeguy56@gmail.com>
2020-11-29 15:30:32 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -49,12 +49,80 @@ int main(){
     }
   }
 }
-void titleInterface(){//going to be the main title with some ascii art and options
+
+int findHighscore(){
+//this should be a sorting algorithm that runs at the start to pass the highscore from highscore.dat to the titlescreen.
+}
+
+void titleInterface(int highScore){//going to be the main title with some ascii art and options
 //load game
 //new game
 //exit
+  bool exitCondition = false;
+  int titleSelect;
+  while(exitCondition == false){
+    cout << R"(
+              _                 _                  
+     /\      | |               | |                 
+    /  \   __| |_   _____ _ __ | |_ _   _ _ __ ___ 
+   / /\ \ / _` \ \ / / _ \ '_ \| __| | | | '__/ _ \
+  / ____ \ (_| |\ V /  __/ | | | |_| |_| | | |  __/ 
+ /_/    \_\__,_| \_/ \___|_| |_|\__|\__,_|_|  \___|....._      
+                                                    `.   ``-.                               .-----.._
+                                                      `,     `-.                          .:      /`
+                                                        :       `"..                 ..-``       :
+                                                        /   ...--:::`n            n.`::...       :
+       Type 1 to Start New Game                       `:``      .` ::          /  `.     ``---..:.
+       Type 2 to Load Game                             `\    .`   ._:    .-:   ::    `.     .-``
+       Type 3 to Quit                                     :  :    :_\\_/: :  .::      `.  /
+                                                            : /      \-../:/_.`-`         \ :
+                                                             :: _.._  q` p ` /`             \|
+                                                             :-`    ``(_. ..-----hh``````/-._:
+                                                                         `:      ``     /     `
+                                                                         E:            /
+                                                                          :          _/
+                                                                          :    _..-``
+                                                                           l--``)" << endl;
+    cout << "Current highscore is" << endl;
+    cin >> titleSelect;
+    switch (titleSelect)
+    {
+    case 1:
+      /* code */
+      break;
+
+    case 2:
+      /* code */
+      break;
+
+    case 3:
+      /* code */
+      break;
+
+    default:
+      break;
+    }
+  }
 
 }
+
+void newGame(){
+  string name;
+  string option = "n";
+  cout << string(50, '\n');
+  while(option != "y", option != "Y"){
+    cout << "What is your name, adventurer?" << endl;
+    cin >> name;
+    cout << "Ah, so you are the one they call " << name << "is that right?" << endl;
+    cout << "(Y/N)" << endl;
+    cin >> option;
+  }
+  cout << "Wonderful, Wonderful... Welcome to the town of Rathamir, many adventurers like yourself have tried our challenge." << endl;
+  cout << "In our town is, the spire. The spire is an infinitely tall tower of challenges for adventurers. We wish you luck on your adventure." << endl;
+  
+
+}
+
 int loadSave(){
 //loads the game's save then goes to gameplay
 }
